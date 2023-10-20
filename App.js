b3a776924a874efa8db17873bc1ef7947eb4b24f
tamana007 <tamana.efatwira1@gmail.com>
2023-10-20 16:41:01 -0700

    done dlet

--- App.js
+++ App.js
@@ -1,9 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [input, setInput] = useState("");
+  const [grocery, setgrocery] = useState([]);
+
+  function handleBtn(e) {
+    e.preventDefault();
+    setgrocery([...grocery, input]);
+    console.log(grocery);
+  }
+
+  function deleteFunc(name) {
+    const updateGrocery=grocery.filter((item) => item !== name);
+    setgrocery(updateGrocery);
+  }
+
+  return (
+    <>
+      <form>
+        <section className="section-center">
+          <div>
+            <h3>Grocery Bud</h3>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+            />
+            <button type="submit" onClick={handleBtn}>
+              submit
+            </button>
+          </div>
+          {grocery.map((item, index) => {
+            const unique=`${item}-${index}`;
+            return (
+              <>
+                <div className="grocery-container" key={unique}>
+                  {item}
+                  <button className="btn">
+                    edit
+                  </button>
+                  <button
+                    className="btn"
+                    onClick={()=>{deleteFunc(item)}}
+                    
+                  >
+                    delete
+                  </button>
+                </div>
+                {/* <div className="btn-container">
+            <button className="btn" onClick={deleteFunc}>delete</button></div> */}
+              </>
+            );
+          })}
+          <div>
+          <button className="btn" onClick={()=>{setgrocery([])}}>Clear Items</button>
+          </div>
+        </section>
+      </form>
+    </>
+  );
 }
 
-export default App
+export default App;
