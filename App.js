89d9542d9072c23230e6c34816ee8afd74b8c072
tamana007 <tamana.efatwira1@gmail.com>
2023-10-21 14:29:31 -0700

    lets see

--- App.js
+++ App.js
@@ -3,56 +3,104 @@ import List from "./List";
 import Alert from "./Alert";
 
 function App() {
-  const [input, setInput] = useState("");
-  const [grocery, setgrocery] = useState([]);
+  //::::::::::::::::::::::::::Tuturial::::::::::::::::::
 
-  function handleBtn(e) {
+  const [name,setName]=useState();
+  const [list,setList]=useState([{Id:"009",Name:"Ali"}]);
+  const[isEdit,setIsEdit]=useState(false);
+  const[editId,setEditId]=useState('');
+  const[alert,setAlert]=useState({show:false,msg:'',type:''});
+
+  //::::::::::::::My Own practice:::::::::::::::::::::::
+  // const [input, setInput] = useState("");
+  // const [grocery, setgrocery] = useState([]);
+
+  // function handleBtn(e) {
+  //   e.preventDefault();
+  //   setgrocery([...grocery, input]);
+  //   console.log(grocery);
+  // }
+
+  // function deleteFunc(name) {
+  //   const updateGrocery = grocery.filter((item) => item !== name);
+  //   setgrocery(updateGrocery);
+  // }
+  // function editFunc(mname){
+  //   const edit=grocery.map((item)=>mname===item);
+  //   setgrocery(edit);
+  // }
+
+  function handleSubmit(e){
     e.preventDefault();
-    setgrocery([...grocery, input]);
-    console.log(grocery);
-  }
+    if(!name){
+      //display: Alert
+    }
+    else if(name && isEdit){
+      //edit
 
-  function deleteFunc(name) {
-    const updateGrocery = grocery.filter((item) => item !== name);
-    setgrocery(updateGrocery);
-  }
-  function editFunc(mname){
-    const edit=grocery.map((item)=>mname===item);
-    setgrocery(edit);
+    }
+    else{
+      const newItem={id:new Date().getTime().toString(),Name:'name'};
+      setList([...list,newItem]);
+      console.log(list);
+    }
+
   }
 
   return (
-    <>
-      <form>
-        <section className="section-center">
-          <div>
-            <h3>Grocery Bud</h3>
-            <input
-              type="text"
-              value={input}
-              onChange={(e) => {
-                setInput(e.target.value);
-              }}
-            />
-            <button type="submit" onClick={handleBtn}>
-              submit
-            </button>
-          </div>
-          <List functionm={deleteFunc} grocery={grocery}/>
-          <div>
-            <button
-              className="btn"
-              onClick={() => {
-                setgrocery([]);
-              }}
-            >
-              Clear Items
-            </button>
-          </div>
-        </section>
-      </form>
-    </>
-  );
+    
+  //  {/* //::::::::::::::::::::::::::::::MY OWN practice:::::::::::::::::::
+    // <>
+    //   <form>
+    //     <section className="section-center">
+    //       <div>
+    //         <h3>Grocery Bud</h3>
+    //         <input */}
+    // {/* //           type="text"
+    //           value={input}
+    //           onChange={(e) => { */}
+    // {/* //             setInput(e.target.value);
+    //           }}
+    //         />
+    //         <button type="submit" onClick={handleBtn}>
+    //           submit
+    //         </button>
+    //       </div> */}
+    // {/* //       <List functionm={deleteFunc} grocery={grocery}/>
+    //       <div>
+    //         <button */}
+    // {/* //           className="btn"
+    //           onClick={() => { */}
+    // {/* //             setgrocery([]);
+    //           }} */}
+    // {/* //         > */}
+    // {/* //           Clear Items */}
+    // {/* //         </button> */}
+    // {/* //       </div> */}
+    // {/* //     </section> */}
+    // {/* //   </form> */}
+    // {/* // </> */}
+  // {/* // );
+  // ) */}
+<>
+<section className="section-center">
+  <form className="grocery-form" onSubmit={handleSubmit}>
+    {alert.show && <Alert/>}
+    <h3>Grocery Bud</h3>
+    <div className="form-control">
+      <input className="grocery" type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/>
+      <button type="submit" className="submit-btn">{isEdit?'Edit':'Submit'}</button>
+    </div>
+  <div className="grocery-container">
+  <List/>
+  </div>
+  <button className="clear-btn" type="submit">Clear All</button>
+  </form>
+ 
+</section>
+
+</>
+  )
 }
 
 export default App;
--- List.js
+++ List.js
@@ -1,33 +1,42 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
-const List = ({ functionm, grocery }) => {
-  return (
-    <>
-      {grocery.map((item, index) => {
-        const unique = `${item}-${index}`;
-        return (
-          <>
-            <div className="grocery-container">
-              {item}
-              <button className="btn">edit</button>
-              <button
-                className="btn"
-                onClick={() => {
-                  functionm(item);
-                }}
-              >
-                delete</button>
-            </div>
-            {/* <div className="btn-container">
-            <button className="btn" onClick={deleteFunc}>delete</button></div> */}
-          </>
-        );
-      })}
-    </>
-  );
+// const List = ({ functionm, grocery }) => {
+//   return (
+//     <>
+//       {grocery.map((item, index) => {
+//         const unique = `${item}-${index}`;
+//         return (
+//           <>
+//             <div className="grocery-container">
+//               {item}
+//               <button className="btn">edit</button>
+//               <button
+//                 className="btn"
+//                 onClick={() => {
+//                   functionm(item);
+//                 }}
+//               >
+//                 delete</button>
+//             </div>
+//             {/* <div className="btn-container">
+//             <button className="btn" onClick={deleteFunc}>delete</button></div> */}
+//           </>
+//         );
+//       })}
+//     </>
+//   );
 
-  // <h2>list component</h2>
-};
+//   // <h2>list component</h2>
+// };
+
+const List=()=>{
+
+return(
+  <>
+  <h2>List Component</h2>
+  </>
+)
+}
 
 export default List;
